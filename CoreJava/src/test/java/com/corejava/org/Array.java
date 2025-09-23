2a63a822bdf0cb405eba158d964aa3ac3fa4d645
Amit Kumar Chaudhary <117908308+amitchaudhary61900@users.noreply.github.com>
2025-09-24 00:56:18 +0530

    Update Array.java

--- CoreJava/src/test/java/com/corejava/org/Array.java
+++ CoreJava/src/test/java/com/corejava/org/Array.java
@@ -1,5 +1,16 @@
-package com.corejava.org;
+import java.util.*;
 
-public class Array {
-
+class Array {
+    public static void main(String[] args) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(1);
+        
+        System.out.println(list); 
+        
+    }
 }
