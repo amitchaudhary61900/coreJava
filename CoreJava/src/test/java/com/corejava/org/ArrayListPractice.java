d7b296d8a46a82056dbb1d9e6e2e214949d57a7a
Amit Kumar Chaudhary <117908308+amitchaudhary61900@users.noreply.github.com>
2025-09-25 12:06:11 +0530

    Update ArrayListPractice.java

--- CoreJava/src/test/java/com/corejava/org/ArrayListPractice.java
+++ CoreJava/src/test/java/com/corejava/org/ArrayListPractice.java
@@ -1,1 +1,22 @@
 
+//Collection - Arraylist
+import java.util.*;
+
+class Main {
+    
+    public static List<Integer> arraylist(){
+          List<Integer> list = new ArrayList<>();
+        
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(1);
+        return list;
+    }
+    
+    public static void main(String[] args) {
+      
+        System.out.println(arraylist());
+        
+    }
+}
